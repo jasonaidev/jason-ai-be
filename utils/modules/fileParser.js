fd2193d8ee8105540c6d218bac44cd55199f79e3
muhammad-ahsan099 <taseries260@gmail.com>
2024-04-05 22:53:16 +0500

    add txt files

--- utils/modules/fileParser.js
+++ utils/modules/fileParser.js
@@ -4,25 +4,38 @@ const pdf = require('pdf-parse');
 /**
  * Extracts text from a PDF available at a given URL.
  * 
- * @param {string} pdfUrl - The URL of the PDF file.
+ * @param {string} fileUrl - The URL of the PDF file.
  * @returns {Promise<string>} The extracted text as a string.
  */
-async function fileParser(pdfUrl) {
+async function fileParser(fileUrl) {
   try {
-    // Fetch the PDF file
+    // Determine the file type based on the URL extension
+    const fileType = fileUrl.split('.').pop().toLowerCase();
+
+    // Fetch the file
     const response = await axios({
-      url: pdfUrl,
+      url: fileUrl,
       method: 'GET',
-      responseType: 'arraybuffer', // Important to handle the PDF binary data
+      responseType: 'arraybuffer', // Suitable for binary data
     });
 
-    // Use pdf-parse to extract text from the PDF buffer
-    const data = await pdf(response.data);
-    return data.text;
+    // Process based on file type
+    if (fileType === 'pdf') {
+      // Use pdf-parse to extract text from the PDF buffer
+      const data = await pdf(response.data);
+      return data.text;
+    } else if (fileType === 'txt') {
+      // Convert the ArrayBuffer to string for a text file
+      const text = Buffer.from(response.data).toString('utf8');
+      return text;
+    } else {
+      throw new Error('Unsupported file type');
+    }
   } catch (error) {
-    console.error('An error occurred while extracting text from PDF URL:', error);
+    console.error('An error occurred while extracting text:', error);
     throw error;
   }
 }
+
 
 module.exports = { fileParser };

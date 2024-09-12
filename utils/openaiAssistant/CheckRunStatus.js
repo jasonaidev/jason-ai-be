17e88d7ee277321cb48e93e24c00c554f8323148
Shamail Abbas <shamailabbas61@gmail.com>
2024-09-12 11:35:58 -0700

    fixed error

--- utils/openaiAssistant/CheckRunStatus.js
+++ utils/openaiAssistant/CheckRunStatus.js
@@ -1,30 +1,30 @@
-const OpenAI = require('openai');
+const OpenAI = require("openai");
 // @ts-ignore
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 async function CheckRunStatus(params) {
-    try {
+  try {
+    const data = params;
+    const threadId = data.threadId;
+    const runId = data.runId;
 
-        const data = params;
-        const threadId = data.threadId;
-        const runId = data.runId;
+    // Log the received thread ID and run ID for debugging
+    // console.log(`Received request with threadId: ${threadId} and runId: ${runId}`);
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-        // Log the received thread ID and run ID for debugging
-        console.log(`Received request with threadId: ${threadId} and runId: ${runId}`);
+    // Retrieve the status of the run for the given thread ID and run ID using the OpenAI API
+    const runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
 
-        // Retrieve the status of the run for the given thread ID and run ID using the OpenAI API
-        const runStatus = await openai.beta.threads.runs.retrieve(threadId, runId);
-
-        // Log the retrieved run status for debugging
-        console.log(`Retrieved run status: ${runStatus.status}`);
-        return runStatus
-    } catch (error) {
-        console.error('Error at Check Run Status Assistant Api:', error);
-        throw new Error(error);
-    }
-
+    // Log the retrieved run status for debugging
+    console.log(`Retrieved run status: ${runStatus.status}`);
+    await delay(4000);
+    return runStatus;
+  } catch (error) {
+    console.error("Error at Check Run Status Assistant Api:", error);
+    throw new Error(error);
+  }
 }
 
-module.exports = { CheckRunStatus }
+module.exports = { CheckRunStatus };
